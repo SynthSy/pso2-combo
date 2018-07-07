64e1e45d4cb2f0c2a29306a722f8c0ff946df166
Unknown <synthsy@gmail.com>
2018-07-07 16:00:51 -0700

    change to rawgit

--- translator.user.js
+++ translator.user.js
@@ -11,7 +11,7 @@
 // @downloadURL  https://github.com/SynthSy/pso2-combo/raw/master/translator.user.js
 // ==/UserScript==
 
-var newJSFile = "https://synthsy.github.io/pso2-combo/files/main.bundle.js";
+var newJSFile = "https://cdn.rawgit.com/SynthSy/pso2-combo/master/files/main.bundle.js";
 
 var oldJSFile = "main.914577bef226e3b0e84f.bundle.js";
 
