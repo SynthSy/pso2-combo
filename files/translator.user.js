123e4f2aa4385838943af27dfcbb1bfc2c2b651a
SynthSy <synthsy@gmail.com>
2018-07-07 14:50:52 -0700

    Update translator.user.js

--- files/translator.user.js
+++ files/translator.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         PSO2 Combo Simulator Translator
-// @version      0.5
+// @version      0.6
 // @description  Translates the new damage sim
 // @author       SynthSy
 // @match        https://pso2c.kyata.work/*
