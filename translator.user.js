26982deba45af7c3e3fbc5deddf74c2849d8ef45
Unknown <synthsy@gmail.com>
2018-07-11 03:24:44 -0700

    Uploading a new version

--- translator.user.js
+++ translator.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         PSO2 Combo Simulator Translator
-// @version      1.0.1
+// @version      1.0.2
 // @description  Translates the new damage sim
 // @author       SynthSy
 // @match        https://pso2c.kyata.work/*
@@ -13,7 +13,7 @@
 
 var newJSFile = "https://cdn.rawgit.com/SynthSy/pso2-combo/f02ba1f3a51a983dd327853b281eae6dd16c7ab6/files/main.min.js";
 
-var oldJSFile = "main.914577bef226e3b0e84f.bundle.js";
+var oldJSFile = "main.1b300ee2ee9a66311564.bundle.js";
 
 var pattern = new RegExp(oldJSFile, "i");
 
