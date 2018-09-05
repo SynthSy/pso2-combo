e2adc6e68197ba3fb9face065c55f15338f87294
SynthSy <synthsy@gmail.com>
2018-09-05 14:38:29 -0700

    Update translator.user.js

--- translator.user.js
+++ translator.user.js
@@ -1,8 +1,9 @@
 // ==UserScript==
-// @name         **DEPRECATED** PSO2 Combo Simulator Translator
-// @version      1.0.4
+// @name         PSO2 Combo Simulator Translator
+// @version      1.0.5
 // @description  Translates the new damage sim
 // @author       SynthSy
+// @match        https://pso2c.kyata.work/*
 // @grant        none
 // @run-at       document-start
 // @namespace    https://arks-layer.com/
@@ -10,5 +11,22 @@
 // @downloadURL  https://github.com/SynthSy/pso2-combo/raw/master/translator.user.js
 // ==/UserScript==
 
+var newJSFile = "https://cdn.rawgit.com/SynthSy/pso2-combo/2a950fed80891bf5e76fcbe4ca819fcdf84dd65b/files/main.min.js";
 
-//Website owner has added english support, thus this script no long will function
+var oldJSFile = "main.1b300ee2ee9a66311564.bundle.js";
+
+var pattern = new RegExp(oldJSFile, "i");
+
+function injectScript(originalPage) {
+    console.log('Replacing original cached file with mod: ', oldJSFile, 'with', newJSFile);
+    var moddedPage = originalPage.replace(pattern, newJSFile);
+    document.open();
+    console.log('Mod installed.');
+    document.write(moddedPage);
+    document.close();
+}
+
+setTimeout(function() {
+    console.log('Finding Japanese Sim files...');
+    injectScript(document.documentElement.outerHTML);
+}, 1111);
